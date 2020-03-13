f0f901b284809eba7e877043a5db4d99f923ad52
Gregory Hawman <g.hawman@gmail.com>
2020-03-13 10:34:42 -0700

    added some tests in checking to make sure elements are displaying.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders an h1 with text Womens World Cup', () => {
+  const { getByText } = render(<App />);
+  getByText(/women's world cup/i);
+});
+
+test('renders an h1 with text Most Searched Players 2019', () => {
+  const { getByText } = render(<App />);
+  getByText(/most searched players 2019/i);
 });
